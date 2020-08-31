54c5a02795637e12b33e1109dbde4fd62c436baf
leegenux <limian22@gmail.com>
2020-08-31 17:11:06 +0800

    add i2c initialization

--- main.c
+++ main.c
@@ -10,10 +10,14 @@
 #include <pic16f18854.h>
 #include "bt.h"
 
-void init_args();
+void init_args(void);
+void initAccelerometer(void);
+void initGyroscope(void);
+void init_i2c(void);
 
 void main(void) {
-    // init_args();
+    init_args();
+    init_i2c(); 
     unsigned int i = 0;
     while (1){
         i++;
@@ -25,19 +29,43 @@ void main(void) {
     return;
 }
 
-void init_args() 
-{
-    ANSELA = 0;
+void init_args(void) {
+    // GPIO??
+    ANSELA = 0;         
     ANSELB = 0;
     TRISA = 0B00011111;
     TRISB=0B11110000;
     TRISC = 0;
+    // ??GPIO??
     LATA = 0B00100000;
     LATB = 0b11111111;
     return;
 }
 
-void initBluetoothUART() {
+void initAccelerometer(void) {
+    
+}
+
+void initGyroscope(void) {
+    
+}
+
+void init_i2c(void) {  
+    // ??I2C????
+    // https://electronics.stackexchange.com/questions/417806/pic16f18877-using-i2c-to-read-sensor-lsm9ds0-value
+    // SSP2???????1
+    SSP2CON1.SSPEN = 0b1;       // ??SSP??
+    SSP2CON1.SSPM = 0b1000;     // i2c??? clock = Fosc/(4*(SSP2ADD+1))
+    // SSP2???????2
+    // SSP3???????3
+    SSP2CON3.PCIE = 0;          // ??stop bit??
+    SSP2CON3.SDAHT = 1;         // SDA???????300ns
+    // SSP2???????
+    SSP2STAT.SMP = 0b1;         // ??????
+    SSP2STAT.CKE = 0b1;         // ??input logic
+}
+
+void initBluetoothUART(void) {
     // Set UART I/O pins
     TRISC7 = 1; // RX as input
     TRISC6 = 0; // TX as output
@@ -48,7 +76,7 @@ void initBluetoothUART() {
     BRG16 = 0;
     SPBRG = 25; // 9600 Baud rate
 
-//    SYNC = 0; // Asynchronous mode
+    SYNC = 0; // Asynchronous mode
     SPEN  = 1;    // Enable serial port pins
 
     // Enable Tx and Rx of UART
