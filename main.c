5d6c7991fe12fd5f54a488b98504a42962d6e101
zengdewei1 <2313970511@qq.com>
2020-09-08 22:40:32 +0800

    蓝牙2.0

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
 #pragma config RSTOSC = HFINT32 // Power-up default value for COSC bits (HFINTOSC with OSCFRQ= 32 MHz and CDIV = 1:1)
 #pragma config WDTE = OFF       // WDT operating mode (WDT Disabled, SWDTEN is ignored)
+#pragma config CLKOUTEN = ON
+#pragma config FEXTOSC = OFF
+// CONFIG1
+#pragma config FEXTOSC = OFF    // External Oscillator mode selection bits (Oscillator not enabled)
+#pragma config CLKOUTEN = ON    // Clock Out Enable bit (CLKOUT function is enabled; FOSC/4 clock appears at OSC2)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (FSCM timer enabled)
+
+// CONFIG2
+#pragma config MCLRE = ON       // Master Clear Enable bit (MCLR pin is Master Clear function)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config LPBOREN = OFF    // Low-Power BOR enable bit (ULPBOR disabled)
+#pragma config BOREN = ON       // Brown-out reset enable bits (Brown-out Reset Enabled, SBOREN bit is ignored)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (VBOR) set to 1.9V on LF, and 2.45V on F Devices)
+#pragma config ZCD = OFF        // Zero-cross detect disable (Zero-cross detect circuit is disabled at POR.)
+#pragma config PPS1WAY = ON     // Peripheral Pin Select one-way control (The PPSLOCK bit can be cleared and set only once in software)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a reset)
+
+// CONFIG3
+#pragma config WDTCPS = WDTCPS_31// WDT Period Select bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled, SWDTEN is ignored)
+#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+
+// CONFIG4
+#pragma config WRT = OFF        // UserNVM self-write protection bits (Write protection off)
+#pragma config SCANE = available// Scanner Enable bit (Scanner module is available for use)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.)
+
+// CONFIG5
+#pragma config CP = OFF         // UserNVM Program memory code protection bit (Program Memory code protection disabled)
+#pragma config CPD = OFF        // DataNVM code protection bit (Data EEPROM code protection disabled)
 
 #include <xc.h>
 #include <pic16f18854.h>
@@ -10,7 +42,7 @@ void set_pps();
 void set_interrupt();
 void init_port();
 void set_eusart();
-void send_data();
+void send_data(char data);
 void recv_data();
 void enable_out();
 void disable_out();
@@ -18,10 +50,11 @@ void out_reset();
 void delay(int delay_time);
 
 unsigned int data = 0;
+char a = 'g';
 
 void __interrupt() irs_routine() {
    //  PERIPHERAL INTERRUPT STATUS REGISTER 0
-   if (PIR0bits.INTF == 1) {
+   if (0){// (PIR0bits.INTF == 1) {
         PIE0bits.INTE = 0;
         disable_out();
         recv_data();
@@ -32,14 +65,16 @@ void __interrupt() irs_routine() {
    }
 }
 
-void send_data(){
-    enable_out();
-    out_reset();
+void send_data(char data){
+   // enable_out();
+   // out_reset();
     do{
-        while (PIR3bits.TXIF != 1);
-        TX1REG = 0b10101100;
+        while (TRMT == 0);
+        TX1REG = data;
+        // while (PIR3bits.TXIF != 1);
+        
     }while(0);
-    disable_out();
+    //disable_out();
 }
 
 void recv_data(){
@@ -59,12 +94,15 @@ void main(void) {
     set_interrupt();
     set_pps();
     set_eusart();
+    //TX1STAbits.TXEN=0;
     while (1)
     {
-        send_data();
-        if (data != 0){
-            LATAbits.LATA5 = 1;
-        }
+       // send_data('a');
+        
+        //TX1STAbits.TXEN=0;
+        while (PIR3bits.TXIF != 1);
+        TX1REG = 'a';
+        //TX1STAbits.TXEN=1;
     }
     
     return;
@@ -101,7 +139,7 @@ void init_oc() {
     // HFINTOSC
     OSCCON1bits.NOSC = 0b000;
     // 32 MHz
-    OSCFRQbits.HFFRQ = 0b0111;
+    OSCFRQbits.HFFRQ = 0b0110;
     // 1x divider
     OSCCON1bits.NDIV = 0b0000;
 }
@@ -116,9 +154,9 @@ void set_interrupt() {
 }
 
 void set_eusart() {
-    BAUD1CONbits.BRG16 = 1;
-    SP1BRGL = 0xCF;
-    SP1BRGH = 0x00;
+    BAUD1CONbits.BRG16 = 0;
+    SP1BRGL = 0x44;
+    //SP1BRGH = 0x03;
 
     //set sync master clock from BRC
     TX1STAbits.CSRC = 1;
@@ -129,6 +167,9 @@ void set_eusart() {
 
     RC1STAbits.SPEN = 1;
     RC1STAbits.CREN = 0;
+    RC1STAbits.SREN = 0;
+    TX1STAbits.TXEN = 1;
+
 }
 
 void set_pps() {
